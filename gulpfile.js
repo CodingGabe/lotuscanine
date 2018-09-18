b97336495ea23a9d168811dba93bc3ef1981cdb7
Gabe Amaya <csstrickster2@gmail.com>
2018-09-18 18:18:33 -0500

    added gulp files

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require("gulp"),
+      sass = require("gulp-sass"),
+      autoprefixer = require("gulp-autoprefixer")
+
+gulp.task('sass', function(){
+    gulp.src("themes/lotus/static/sass/**/*.sass")
+    .pipe(sass({
+        outputStyle : "compressed"
+    }))
+    .pipe(autoprefixer({
+        browsers : ["last 20 versions"]
+    }))
+    .pipe(gulp.dest("themes/lotus/static/css"))
+})
+
+// Watch asset folder for changes
+gulp.task("watch", ["sass"], function () {
+    gulp.watch("themes/lotis/static/sass/**/*", ["sass"])
+})
+
+// Set watch as default task
+gulp.task("default", ["watch"])
